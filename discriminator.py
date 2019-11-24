d2d2436941373820cd90fda6a2fff58e4f4d988f
Okabe9 <sergixghost@gmail.com>
2019-11-24 07:05:41 +0100

    Error discriminator

--- discriminator.py
+++ discriminator.py
@@ -33,5 +33,4 @@ def discriminator_loss(disc_real_output, disc_generated_output):
     generated_loss = loss_object(tf.zeros_like(disc_generated_output, disc_generated_output))
 
     total_disc_loss = real_loss + generated_loss
-
     return total_disc_loss
